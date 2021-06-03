1bef06a5c6cf30941d01279343fb4927d5161fd8
yjzhang3 <yjzhang3@bu.edu>
2021-06-03 18:54:23 -0400

    with scores csv

--- reply_NLP.py
+++ reply_NLP.py
@@ -8,19 +8,20 @@ client = ExpertAiClient()
 language = "en"
 
 
-
 df = pd.read_csv("replies.csv")
 df["sentiment score"] = ""
+score=[]
 
 for index, row in df.iterrows():
     text = row['text'].encode("ascii", "ignore") # make sure all characters are unicode
-    print(text)
+    #print(text)
     output = client.specific_resource_analysis(
-        body={"document": {"text": text}}, 
+        body={"document": {"text": str(text)}}, 
         params={'language': language, 'resource': 'sentiment'
     })
-    print("sentiment:", output.sentiment.overall)
-    df.loc[index] = output.sentiment.overall
-           
-    
+    #print("sentiment:", output.sentiment.overall)
+    score.append(output.sentiment.overall)
+
+df["sentiment score"]= score
+df.to_csv("replies_wscore.csv")
 
