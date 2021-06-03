cf5761fa08d692b65b2696d6eb32092fe91397d1
yjzhang3 <yjzhang3@bu.edu>
2021-06-03 16:45:52 -0400

    form a scraping workflow

--- scraper_replies.py
+++ scraper_replies.py
@@ -23,20 +23,34 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 
-name = '@StarTribune' #'therecount'
-tweet_id = '1397978497902166020'
-replies=[]
-for tweet in tweepy.Cursor(api.search,q='to:{}'.format(name), since_id = tweet_id, tweet_mode='extended').items(50):
-    if hasattr(tweet, 'in_reply_to_status_id_str'):
-        #print("has replies")
-        #print(tweet.in_reply_to_status_id_str)
-        if (tweet.in_reply_to_status_id_str==tweet_id):
-            replies.append(tweet)
-print(len(replies))
-            
-with open('replies.csv', 'w') as f:
-    csv_writer = csv.DictWriter(f, fieldnames=('user', 'text'))
-    csv_writer.writeheader()
-    for tweet in replies:
-        row = {'user': tweet.user.screen_name, 'text': tweet.text.replace('\n', ' ')}
-        csv_writer.writerow(row)
+def scraper_single(name,tweet_id,since_date,end_date):
+    replies=[]
+    for tweet in tweepy.Cursor(api.search,q='to:{}'.format(name), since_id = tweet_id, since=since_date, until=end_date, tweet_mode='extended').items(1000):
+        # search for any tweets that are more recent than this id, and then only within this time peirod 
+        if hasattr(tweet, 'in_reply_to_status_id_str'):
+            #print("has replies")
+            #print(tweet.in_reply_to_status_id_str)
+            if (tweet.in_reply_to_status_id_str==tweet_id):
+                replies.append(tweet)
+    print(len(replies))
+              
+    with open('replies.csv', 'a') as f:
+        csv_writer = csv.DictWriter(f, fieldnames=('user', 'text'))
+        csv_writer.writeheader()
+        for tweet in replies:
+            tweet.full_text
+            row = {'user': tweet.user.screen_name, 'text': tweet.full_text.replace('\n', ' ')}
+            csv_writer.writerow(row)
+
+def scraper_multiple(num_tweet):
+    for j in range(num_tweet):
+        scraper_single(name[j],tweet_id[j],since_date[j],end_date[j])
+
+name = ['@nytimes','@GavinNewsom']
+tweet_id = ['1398028759534551041','1400529612023558144']
+since_date = ['2021-05-27','2021-06-03']
+end_date = ['2021-05-28','2021-06-03']
+
+scraper_multiple(len(name))
+
+        
