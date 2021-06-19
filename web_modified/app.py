88b71ba54a8b336b54d4814d01766ec4c275d7af
Edwin Sun <34005715+edsun123@users.noreply.github.com>
2021-06-18 22:18:38 -0400

    added wrapper functions, cannot fix the hover over

--- web_modified/app.py
+++ web_modified/app.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from scripts import tabledef
-from scripts import forms
-from scripts import helpers
-from flask import Flask, redirect, url_for, render_template, request, session
-import json
-import sys
+from flask import Flask, redirect, url_for, render_template, request, session, Response
 import os
+import io
+import random
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 app = Flask(__name__)
 app.secret_key = os.urandom(12)  # Generic key for dev purposes only
@@ -15,11 +14,29 @@ app.secret_key = os.urandom(12)  # Generic key for dev purposes only
 #from flask_heroku import Heroku
 #heroku = Heroku(app)
 
+array = [0]
+num = 0
+
 # ======== Routing =========================================================== #
 # -------- Login ------------------------------------------------------------- #
 @app.route('/', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
+
+@app.route('/plot.png')
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+def create_figure():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+    axis.plot(xs, ys)
+    return fig
 
 # ======== Main ============================================================== #
 if __name__ == "__main__":
--- web_modified/static/js/scripts.js
+++ web_modified/static/js/scripts.js
@@ -1,11 +1,5 @@
 var dropdown = document.querySelector('#dropdown_1');
-dropdown.addEventListener('click', function(event) {
-  event.stopPropagation();
-  dropdown.classList.toggle('is-active');
-});
-
-var dropdown = document.querySelector('#dropdown_2');
-dropdown.addEventListener('click', function(event) {
+dropdown_1.addEventListener('click', function(event) {
   event.stopPropagation();
   dropdown.classList.toggle('is-active');
 });
