187f629cbd8986bf247cf58ab5391ee09ba2ccd3
Yujia <56132313+yjzhang3@users.noreply.github.com>
2021-06-20 23:26:21 -0400

    delete unnecessary sections

--- pipeline.py
+++ pipeline.py
@@ -4,6 +4,7 @@ from reply_wordCloud import *
 from collections import Counter
 import itertools
 import csv
+
 tweet_list = ["https://twitter.com/therecount/status/1392853246725791745",
               "https://twitter.com/DE_MisesCaucus/status/1397239498111655947",
               "https://twitter.com/GavinNewsom/status/1398025553274277890",
@@ -11,35 +12,8 @@ tweet_list = ["https://twitter.com/therecount/status/1392853246725791745",
               "https://twitter.com/thehill/status/1400119546078433284",
               "https://twitter.com/HawaiiNewsNow/status/1402677284348338177",
               "https://twitter.com/HawaiiNewsNow/status/1402677284348338177"]
+
 state_name = ["New York","Delaware","California","Ohio","West Virginia","Hawaii","New Jersey"]
-f1 = "replies" + state_name[1] + ".csv"
-f2 = "post-replies" + state_name[1] + ".csv"
-scraper(tweet_list[1],f1)
-[l1,l2,l3] = Sent(f1)
-
-scores = sorted(l1) # sort from least to most
-c1 = Counter(l2)
-c2 = Counter(l3)
-
-behav_traits = []
-emot_traits = []
-behav_freq = []
-emot_freq = []
-
-for key in c1.items():
-    behav_traits.append(key[0])
-    behav_freq.append(key[1])
-
-for key in c2.items():
-    emot_traits.append(key[0])
-    emot_freq.append(key[1])
-
-rows = zip(scores,behav_traits,behav_freq,emot_traits,emot_freq)
-with open(f2, "w") as f:
-    writer = csv.writer(f)
-    for row in rows:
-        writer.writerow(row)
-
 
 def driver(t_list,s_list):
     for num in range(len(t_list)):
@@ -51,7 +25,8 @@ def driver(t_list,s_list):
         scores = sorted(l1) # sort from least to most
         c1 = Counter(l2) # count unique elements and frequency
         c2 = Counter(l3)
-
+# (neurtral, 2), (addiction, 3)....
+        
         behav_traits = []
         emot_traits = []
         behav_freq = []
