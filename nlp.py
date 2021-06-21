a8778697b2ff6f1b28ca0d58992973f9fbb794f5
edsun123 <34005715+edsun123@users.noreply.github.com>
2021-06-20 23:01:17 -0400

    Update nlp.py
    cleaned up all the comments

--- nlp.py
+++ nlp.py
@@ -1,42 +1,23 @@
-# -*- coding: utf-8 -*-
-
-from datetime import datetime
-startTime = datetime.now()
-
-LINKS = []
-HASH_TAGS = []
-
-WORDS = []
-
 import os
-os.environ["EAI_USERNAME"] = #username
-os.environ["EAI_PASSWORD"] = #password
-
-from expertai.nlapi.cloud.client import ExpertAiClient
-client = ExpertAiClient()
-language = "en"
-
 import numpy as np
 import matplotlib.pyplot as plt
 import re
+import pandas as pd
+import numpy as np
 from PIL import Image
 from wordcloud import WordCloud, STOPWORDS
 from IPython.display import Image as im
-
-import pandas as pd
-import numpy as np
+from expertai.nlapi.cloud.client import ExpertAiClient
 
 def word_cloud(csv_file):
 
   df= pd.read_csv(csv_file)
-
-  #df['no_of_mentions'] = df['text'].str.count("@")
-
   df['tags_removed'] = df['text'].apply(lambda x : x[x.find(" ")+1: ]+" " )
-  """## Removing Mentions"""
+  
+  ## Removing Mentions
   df['tags_removed'] =  df['tags_removed'].apply(lambda x: x[x.find(" ")+1: ] if ("@" in x) else x)
-  """### Removing links"""
-  #for x in df['text']:
+  
+  ### Removing links
   for x in df['tags_removed']:
     if ("http" in x):
       link = x[x.find("http") : x.find(" ")+1]
@@ -45,15 +26,14 @@ def word_cloud(csv_file):
 
   df['links_removed'] =  df['tags_removed'].apply(lambda x: x[x.find(" ")+1: ] if ("http" in x) else x)
   df = df.drop(columns=['tags_removed'])
-  """Dropping null rows"""
-  #countinh the str len in each row
-  # df['links_removed'].apply(lambda x: len(x))
   df = df.replace("", np.nan)
   df = df.dropna(subset=['links_removed'])
+  
   ## Word Cloud
-  ###**Using the key Phrase extraction feature**
+  ## Using the key Phrase extraction feature
+  
   All_sentences = list(df['links_removed'])
-  #All_sentences
+  
   for sentence in All_sentences:
     document = client.specific_resource_analysis(
       body={"document": {"text": sentence }}, 
@@ -71,8 +51,5 @@ def word_cloud(csv_file):
   plt.imshow(wc, interpolation='bilinear')
   plt.axis("off")
   plt.show()
+  
   return fig
-
-  #print(datetime.now() - startTime)
-
-word_cloud(r"C:\Users\Pravallika Myneni\Desktop\Vaccine-Sent-NLP-word-cloud\replies.csv")
